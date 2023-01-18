5807b2af80cf86a5f36b40be11e4fa8e9e9beed9
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2023-01-19 01:32:23 +0600

    Banner Update

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,140 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="flex flex-row lg:flex-row-reverse my-10 justify-center items-center">
+      <div className="flex flex-col md:flex-row-reverse md:gap-20 gap-4 justify-center">
+        <div className="mx-auto md:mx-0 flex-1">
+          <img
+            src="https://i.ibb.co/0FxzTnM/abdr-pic.png"
+            class="w-[250px] md:w-[350px] lg:w-[400px] shadow-2xl rounded-lg shadow-black "
+            alt=""
+          />
+        </div>
+
+        <div className=" flex-1 text-left mt-8 mx-auto">
+          <h3 className=" te text-gray-500 text-[15px]  rounded-l-full rounded-tr-full  font-medium">
+            Get to Know Me
+          </h3>
+          <h1 className="text-4xl my-2 font-bold">
+            Hi, I'm <span className=" text-orange-500"> Abdur Rahman</span>
+          </h1>
+          <h1 className="text-2xl ">Font End Webdeveloper</h1>
+          <div>
+            <ul>
+              <li className="my-2 flex items-center gap-2">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                    />
+                  </svg>
+                </span>
+
+                <span className="mr-2">
+                  Joypurhat, Rajshahi, Dhaka, Bangladesh
+                </span>
+              </li>
+              <li className="my-2 flex items-center gap-2">
+                <span className="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0l-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z"
+                    />
+                  </svg>
+                </span>
+                <span className="mr-2">+8801763378457</span>
+              </li>
+              <li className="my-2 flex items-center gap-2">
+                <span className="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                    />
+                  </svg>
+                </span>
+                <span className="mr-2">abdurrahmanargon7@gmail.com</span>
+              </li>
+              <li className="my-2 flex items-center gap-2">
+                <span className="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25"
+                    />
+                  </svg>
+                </span>
+                <span className="mr-2">https//www.website.com/</span>
+              </li>
+            </ul>
+          </div>
+
+          {/* <p className="py-6">
+        I am a font end web developer. now i am training in
+        programing-hero's webdevelopement chorse. Now I hope, I can
+        working very easy in a job.
+      </p> */}
+          <button className=" flex items-center gap-2 my-6 py-2 btn bg-green-700">
+            <a href="/">Download Resume</a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
